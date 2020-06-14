b2ab3092367b1770b637d225ce4615e3e79df5f7
Christopher Dosin <hello@christopherdosin.me>
2020-06-14 22:09:20 +0300

    Ability to upload more filetypes

--- src/Resources/app/administration/src/module/sas-esd/component/sas-media-upload/index.js
+++ src/Resources/app/administration/src/module/sas-esd/component/sas-media-upload/index.js
@@ -1,4 +1,17 @@
+import template from './sas-media-upload-v2.html.twig';
+
 Shopware.Component.extend('sas-media-upload', 'sw-media-upload-v2', {
+    template,
+    
+    props: {
+        fileAccept: {
+            type: String,
+            required: false,
+            default: 'image/*'
+        }
+
+    },
+
     methods: {
         getMediaEntityForUpload() {
             const mediaItem = this.mediaRepository.create();
