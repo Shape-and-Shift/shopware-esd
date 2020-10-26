4971ababa5f71c5768280d3a587af5df26533888
Christopher Dosin <hello@christopherdosin.me>
2020-10-26 08:48:00 +0200

    Create zip workflow

--- bin/fix-cs.sh
+++ bin/fix-cs.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-php ../../../dev-ops/analyze/vendor/bin/ecs check --fix --config=../../../vendor/shopware/platform/easy-coding-standard.yml
+php ../../../../dev-ops/analyze/vendor/bin/ecs check --fix --config=../../../../vendor/shopware/platform/easy-coding-standard.yml
