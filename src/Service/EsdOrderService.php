b0c216414bf38d866fa3a75cbc1bd247f4fcd4a3
hung mac <hi@hungmac.com>
2021-04-29 21:59:31 +0700

    Fixed to ignore the lineItem is voucher for the ESD

--- src/Service/EsdOrderService.php
+++ src/Service/EsdOrderService.php
@@ -211,6 +211,10 @@ class EsdOrderService
     public function isEsdOrder(OrderEntity $order): bool
     {
         foreach ($order->getLineItems() as $lineItem) {
+            if (!$lineItem->getProduct()) {
+                continue;
+            }
+
             /** @var EsdEntity $esd */
             $esd = $lineItem->getProduct()->getExtension('esd');
             if (empty($esd)) {
